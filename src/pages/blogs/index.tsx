3fbb6c71037208ce4ab1e814b2de9520637c7c82
THAIRUNG\krishanathep.j <krishanathep@gmail.com>
2025-03-31 17:01:44 +0700

    include mantine datatable

--- src/pages/blogs/index.tsx
+++ src/pages/blogs/index.tsx
@@ -1,14 +1,36 @@
-import { useEffect } from "react";
-import { Link } from 'react-router-dom'
+import { useState, useEffect } from "react";
+// import { Link } from 'react-router-dom'
 import dayjs from "dayjs";
 import { useBlogStore } from "../../stores/blogStrore";
+import { DataTable } from "mantine-datatable";
+
+const PAGE_SIZES = [10, 20, 30];
 
 const Blogs = () => {
   // Use The Zustand store
-  const { blogs, isLoading, error, fetchBlogs } = useBlogStore();
+  const { blogs, fetchBlogs } = useBlogStore();
+
+  const [pageSize, setPageSize] = useState(PAGE_SIZES[0]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(true);
+  const [records, setRecords] = useState(blogs.slice(0, pageSize));
 
   useEffect(() => {
-    fetchBlogs()
+    setPage(1);
+  }, [pageSize]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const from = (page - 1) * pageSize;
+      const to = from + pageSize;
+      setRecords(blogs.slice(from, to));
+      setLoading(false);
+    };
+    getData();
+  }, [page, pageSize, blogs]); // Add dependencies for page, pageSize, and blogs
+
+  useEffect(() => {
+    fetchBlogs();
   }, []);
 
   return (
@@ -37,76 +59,93 @@ const Blogs = () => {
               <div className="col-md-12">
                 <div className="card">
                   <div className="card-body">
-                    <div className="row">
-                      <div className="col-md-12">
-                        <div className="float-right">
-                          <Link to={'/blogs/create'} className="btn btn-primary mb-2">
-                            + Create blog
-                          </Link>
-                        </div>
-                      </div>
-                    </div>
-                    
-                    {error && (
-                      <div className="alert alert-danger">{error}</div>
-                    )}
-                    
-                    {isLoading ? (
-                      <div className="text-center my-3">
-                        <div className="spinner-border text-primary" role="status">
-                          <span className="sr-only">Loading...</span>
-                        </div>
-                      </div>
-                    ) : (
-                      <table className="table table-bordered">
-                        <thead>
-                          <tr>
-                            <th>ID</th>
-                            <th>Image</th>
-                            <th>Title</th>
-                            <th>Content</th>
-                            <th>Author</th>
-                            <th>Created at</th>
-                            <th>Actions</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          {blogs.map((blog, index) => (
-                            <tr key={blog.id}>
-                              <td>{index + 1}</td>
-                              <td>
-                                <img
-                                  className="img-thumbnail"
-                                  src={
-                                    "https://full-stack-app.com/laravel_auth_jwt_api/public/uploads/" +
-                                    blog.image
-                                  }
-                                  alt={blog.title}
-                                  width={"50"}
-                                />
-                              </td>
-                              <td>{blog.title}</td>
-                              <td>{blog.content}</td>
-                              <td>{blog.author}</td>
-                              <td>
-                                {dayjs(blog.created_at).format("DD-MMM-YYYY")}
-                              </td>
-                              <td>
-                                <button className="btn btn-primary btn-sm">
-                                  <i className="fas fa-eye"></i>
-                                </button>{" "}
-                                <button className="btn btn-info btn-sm">
-                                  <i className="fas fa-edit"></i>
-                                </button>{" "}
-                                <button className="btn btn-danger btn-sm">
-                                  <i className="fas fa-trash"></i>
-                                </button>
-                              </td>
-                            </tr>
-                          ))}
-                        </tbody>
-                      </table>
-                    )}
+                    <DataTable
+                      style={{
+                        fontFamily: "Prompt",
+                      }}
+                      withBorder
+                      withColumnBorders
+                      verticalSpacing="md"
+                      fontSize={"md"}
+                      records={records}
+                      fetching={loading}
+                      idAccessor="id"
+                      minHeight={200}
+                      totalRecords={blogs.length}
+                      recordsPerPage={pageSize}
+                      page={page}
+                      onPageChange={(p) => setPage(p)}
+                      recordsPerPageOptions={PAGE_SIZES}
+                      onRecordsPerPageChange={setPageSize}
+                      paginationColor="gray"
+                      paginationSize="md"
+                      columns={[
+                        {
+                          accessor: "index",
+                          title: "#",
+                          textAlignment: "center",
+                          width: 80,
+                          render: (record) => records.indexOf(record) + 1,
+                        },
+                        {
+                          accessor: "title",
+                          title: "ชื่อเรื่อง",
+                          titleStyle: { textAlign: "center" },
+                        },
+                        {
+                          accessor: "content",
+                          title: "รายละเอียด",
+                          titleStyle: { textAlign: "center" },
+                        },
+                        {
+                          accessor: "author",
+                          title: "ผู้แต่ง",
+                          titleStyle: { textAlign: "center" },
+                        },
+                        {
+                          accessor: "image",
+                          title: "รูปภาพ",
+                          textAlignment: "center",
+                          render: ({image}) => (
+                            <>
+                              <img src={ "https://full-stack-app.com/laravel_auth_jwt_api/public/uploads/" + image} alt="" width={'100'} />
+                            </>
+                          )
+                        },
+                        {
+                          accessor: "created_at",
+                          title: "วันที่สร้าง",
+                          titleStyle: { textAlign: "center" },
+                          render: ({ created_at }) =>
+                            dayjs(created_at).format("DD-MMM-YYYY"),
+                        },
+                        {
+                          accessor: "updated_at",
+                          title: "วันที่แก้ไข",
+                          titleStyle: { textAlign: "center" },
+                          render: ({ updated_at }) =>
+                            dayjs(updated_at).format("DD-MMM-YYYY"),
+                        },
+                        {
+                          accessor: "actions",
+                          title: "ดำเนินการ",
+                          textAlignment: "center",
+                          render: () => (
+                            <>
+                              <button className="btn btn-primary btn-sm">
+                                <i className="fas fa-eye"></i>
+                              </button>{" "}
+                              <button className="btn btn-info btn-sm">
+                                <i className="fas fa-edit"></i>
+                              </button>{" "}
+                              <button className="btn btn-danger btn-sm">
+                                <i className="fas fa-trash"></i>
+                              </button>
+                            </>
+                          ),
+                        },
+                      ]}
+                    />
                   </div>
                 </div>
               </div>
